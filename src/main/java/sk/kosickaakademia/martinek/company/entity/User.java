1d8925396f9a5b36a3715b48659081ec4c946515
danielista <61905525+danielista@users.noreply.github.com>
2021-03-02 10:40:50 +0100

    10:40 contstructors and getters

---
+++ src/main/java/sk/kosickaakademia/martinek/company/entity/User.java
@@ -0,0 +1,43 @@
+package sk.kosickaakademia.martinek.company.entity;
+
+import sk.kosickaakademia.martinek.company.enumartoris.Gender;
+
+public class User {
+    public User(int id, String fname, String lname, int age, int gender) {
+        this(fname,lname,age,gender);  // tento konštruktor si vola toho druheho koli skratenemu zapisu ;)
+        this.id = id;
+    }
+
+    private int id;
+    private String fname;
+    private String lname;
+    private int age;
+    private Gender gender;
+
+    public User(String fname, String lname, int age, int gender) {
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.gender = gender==0?Gender.MALE:gender==1?Gender.FEMALE:Gender.OTHER;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Gender isGender() {
+        return gender;
+    }
+}
---
+++ src/main/java/sk/kosickaakademia/martinek/company/enumartoris/Gender.java
@@ -0,0 +1,5 @@
+package sk.kosickaakademia.martinek.company.enumartoris;
+
+public enum Gender {
+    MALE, FEMALE, OTHER
+}
