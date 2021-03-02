49380d347b2bf3cb514866df4863dc63c67f3386
danielista <61905525+danielista@users.noreply.github.com>
2021-03-02 09:45:35 +0100

    perší komit mavenu

---
+++ src/main/java/sk/kosickaakademia/martinek/company/App.java
@@ -0,0 +1,19 @@
+package sk.kosickaakademia.martinek.company;
+
+import org.json.simple.*;
+/**
+ * Hello world! helllo
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+        JSONObject obj = new JSONObject();
+
+
+    }
+}
